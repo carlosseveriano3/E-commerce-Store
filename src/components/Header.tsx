84e38f679fa06290ea51bca5de9ddd71339faa44
carlosseveriano3 <carlosseveriano3@gmail.com>
2024-05-24 04:33:07 -0300

    continuing Header 24/05/24

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,37 +1,63 @@
 'use client'
 
+import Link from "next/link"
 import Image from "next/image"
+import { useState } from "react"
+
+
 import Logo from '../../public/book.png'
 import Menu from '../../public/assets/Menu.svg'
 import User from '../../public/assets/User.svg'
 import Favorites from '../../public/assets/Favorites.svg'
 import Cart from '../../public/assets/Cart.svg'
 import Search from '../../public/assets/Search.svg'
-
-const navLinks = [
-  {name: "Livros"},
-  {name: "Livros de domínio público"},
-  {name: "Categorias"},
-  {name: "contato"}
-]
+import Close from '../../public/assets/Close.svg'
 
 export function Header() {
 
-  function openMenu() {
-    
+  const [isOpen, setIsOpen] = useState(false);
+
+  function getMenuClasses() {
+    let menuClasses: string[] = []
+
+    if (isOpen) {
+      menuClasses = [
+        "flex",
+        "absolute",
+        "bg-slate-600",
+        "w-full",
+        "p-4",
+        "left-0",
+        "flex-col",
+        "items-center",
+      ]
+    }
+
+    return menuClasses.join(" ")
   }
 
   return(
     <nav>
-      <div className="bg-slate-600 p-2 flex items-center justify-between">
-          <div className="ml-2">
-            <Image 
-            src={Menu}
-            alt="Menu"
-            className="size-8"
-            onClick={openMenu}
-            />
-          </div>
+      <div className="bg-slate-600 w-full p-2 flex items-center justify-between sm:p-3">
+          <button 
+            className="ml-2 md:hidden"
+            onClick={() => {
+              setIsOpen(!isOpen)
+            }}  
+          >
+            {isOpen ? (
+              <Image src={Close} 
+              alt="Close" 
+              className="size-8"
+              />
+            ) : (
+              <Image 
+              src={Menu} 
+              alt="Menu" 
+              className="size-8"
+              />
+            )}
+          </button>
 
         <div className="flex items-center gap-2">
           <div className="">
@@ -46,7 +72,7 @@ export function Header() {
           </div>
         </div>
 
-        <div className="flex gap-2 mr-2">
+        <div className="flex gap-2">
           <div className="">
             <Image 
             src={User}
@@ -65,7 +91,7 @@ export function Header() {
             <span className="hidden text-center">favoritos</span>
           </div>
 
-          <div>
+          <div className="mr-2">
           <Image 
             src={Cart}
             alt="user"
@@ -77,9 +103,9 @@ export function Header() {
 
       </div>
 
-      <div className="bg-amber-400 h-12 flex justify-center">
+      <div className={`${isOpen ? "" : "bg-amber-400"} h-12 flex justify-center`}>
         
-        <form className="flex items-center focus-within:text-black">
+        <form className= "flex items-center focus-within:text-black md:absolute md:top-4 md:ml-10">
           <div className="relative text-black">
             <Image 
               src={Search}
@@ -90,23 +116,25 @@ export function Header() {
               type="text" 
               autoComplete="off" 
               placeholder="O que você procura?"
-              className="px-3 py-[4px] w-[360px] outline-none rounded-md border-none placeholder-gray-400 ring-2 ring-gray-300 focus:ring-gray-500 focus:ring-2"
+              className="px-3 py-[4px] w-[360px] outline-none rounded-md border-none 
+              placeholder-gray-400 ring-2 ring-gray-300 focus:ring-gray-500 focus:ring-2"
             />
           </div>
         </form>
 
-        {/* <div className="flex lg:w-3/4 sm:w-full sm:pl-4 space-x-52">
-          <ul className="flex gap-7">
-            <li className="hidden text-black md:block">Livros</li>
-            <li className="hidden text-black md:block">Livros de domínio público</li>
-            <li className="hidden text-black md:block">Categorias</li>
-            <li className="hidden text-black md:block">Contato</li>
-          </ul>
-          
-          <div className="">
-            <span className="hidden text-black md:block">Minha estante de livros</span>
+        <div className={`${getMenuClasses()} ${isOpen? "": "hidden"} space-y-6 md:w-full md:flex md:gap-6 md:justify-center md:space-y-0 md:items-center md:translate-x-16`}>
+          <div className="flex flex-col space-y-6 -translate-x-16 md:space-y-0 md:flex-row md:gap-6 md:items-center">
+            <Link href="/">Livros</Link>
+            <Link href="/">Livros de domínio público</Link>
+            <Link href="/">Categorias</Link>
+            <Link href="/">Contato</Link>
           </div>
-        </div> */}
+          
+          <div className="-translate-x-[72px]">
+            <span className=""><Link href="/">Minha estante de livros</Link></span>
+          </div>
+        </div>
+
       </div>
 
     </nav>
